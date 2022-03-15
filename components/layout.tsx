7be84f0ea94811cfd66201023a734d1c5c3804cc
Noe Thalheim <noe.thalheim@gymkirchenfeld.ch>
2022-03-15 14:16:14 +0100

    fix(layout): backgroud screensize problem
    Sometimes the background would become white based on the
    content size.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 import { FunctionComponent } from 'react'
+import Mail from '../components/mail'
 
 type LayoutProbs = {}
 
 const Layout: FunctionComponent<LayoutProbs> = ({children}) => {
     return (
-      <div className="h-max flex flex-col bg-pink-light divide-y">
-        <header className='flex px-20 pt-10'>
+      <div className="h-screen flex flex-col divide-y divide-black">
+        <header className='flex px-20 pt-10 bg-pink-light '>
           <h2>INNOSCOUT WEBSITE V1.1</h2>
           <span className='grow'></span>
           <Link href="/">
@@ -17,12 +18,16 @@ const Layout: FunctionComponent<LayoutProbs> = ({children}) => {
               <a>WIR</a>
           </Link>
         </header>
-        <main className='grow p-20'>
+        <main className='grow p-20 h-max bg-pink-light'>
           {children}
         </main>
         <footer className='bg-pink-light flex px-20 pb-10'>
           <span className='grow'></span>
           <h2>COPYLEFT BLABLA</h2>
+          <span className='p-5'></span>
+          <div className='place-self-end'>
+            <Mail mailto='mailto:innovationscouts@gymkirchenfeld.ch'></Mail>
+          </div>
         </footer>
       </div>
     ) 
