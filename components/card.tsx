249436f0548811255679e179e59286b92f3d40ce
Noe Thalheim <noe.thalheim@gymkirchenfeld.ch>
2022-04-05 15:54:51 +0200

    style: increase card width
    Markdown content need more space

--- components/card.tsx
+++ components/card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 const Card: FunctionComponent<CardProps> = ({title, children}) => {
   return (
     <>
-        <div className='flex flex-col w-44 h-min border-2 border-black '>
+        <div className='flex flex-col w-64 h-min border-2 border-black '>
             <div className='flex bg-pink-dark px-1'>
                 <h3 className='text-white'>{title.toUpperCase()}</h3>
                 <span className='grow'></span>
